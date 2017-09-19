be85d915e26101b99cd2205c48d2534404d839d2
Alejandro Perdomo <aperdomobo@gmail.com>
2017-09-19 10:42:30 -0500

    add code review suggestions

--- test/GithubApi.Gist.test.js
+++ test/GithubApi.Gist.test.js
@@ -26,40 +26,62 @@ describe('Given a github user', () => {
       }
     };
 
-    it('then a new gist should be created', () =>
-      agent
+    let newGistQuery;
+
+    before(() => {
+      newGistQuery = agent
         .post(`${urlBase}/gists`, createGist)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .then((response) => {
-          gist = response.body;
-          expect(response.status).to.equal(statusCode.CREATED);
-          expect(createGist.description).to.equal(gist.description);
-          expect(createGist.public).to.equal(gist.public);
-          expect(createGist.files['promise.js'].content).to.equal(gist.files['promise.js'].content);
-        }));
+        .auth('token', process.env.ACCESS_TOKEN);
+    });
 
-    it('and the Gits should be accessible', () =>
-      agent
-        .get(gist.url)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .then((response) => {
-          expect(response.status).to.equal(statusCode.OK);
-        }));
+    it('then a new gist should be created', () =>
+      newGistQuery.then((response) => {
+        gist = response.body;
+        expect(response.status).to.equal(statusCode.CREATED);
+        expect(createGist.description).to.equal(gist.description);
+        expect(createGist.public).to.equal(gist.public);
+        expect(createGist.files['promise.js'].content).to.equal(gist.files['promise.js'].content);
+      }));
 
-    it('and delete the current gist', () =>
-      agent
-        .del(gist.url)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .then((response) => {
-          expect(response.status).to.equal(statusCode.NO_CONTENT);
-        }));
+    describe('and get the new gist', () => {
+      let gistQuery;
 
-    it('and the Gits should not be accessible', () =>
-      agent
-        .get(gist.url)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .catch((response) => {
-          expect(response.status).to.equal(statusCode.NOT_FOUND);
-        }));
+      before(() => {
+        gistQuery = agent
+          .get(gist.url)
+          .auth('token', process.env.ACCESS_TOKEN);
+      });
+
+      it('then the Gits should be accessible', () =>
+        gistQuery.then(response => expect(response.status).to.equal(statusCode.OK)));
+
+      describe('when delete a gist', () => {
+        let deleteGistQuery;
+
+        before(() => {
+          deleteGistQuery = agent
+            .del(gist.url)
+            .auth('token', process.env.ACCESS_TOKEN);
+        });
+
+        it('then the gist should be deleted', () =>
+          deleteGistQuery
+            .then(response => expect(response.status).to.equal(statusCode.NO_CONTENT)));
+      });
+
+      describe('and try to get the delete gist', () => {
+        let gistNotFoundQuery;
+
+        before(() => {
+          gistNotFoundQuery = agent
+            .get(gist.url)
+            .auth('token', process.env.ACCESS_TOKEN);
+        });
+
+        it('then the Gits should not be accessible', () =>
+          gistNotFoundQuery
+            .catch(response => expect(response.status).to.equal(statusCode.NOT_FOUND)));
+      });
+    });
   });
 });
