ab45e9bafad5e0f92d7933002590884b33eef770
Alejandro Perdomo <aperdomobo@gmail.com>
2017-09-19 08:07:34 -0500

    add code review suggestions

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -2,35 +2,64 @@ const agent = require('superagent-promise')(require('superagent'), Promise);
 const statusCode = require('http-status-codes');
 const { expect, assert } = require('chai');
 
-describe.only('Given a user github logged', () => {
+describe('Given a user github logged', () => {
   const urlBase = 'https://api.github.com';
   const username = 'germandavid85';
 
   describe('when wanna follow a user', () => {
-    it('then should be a response', () =>
-      agent.put(`${urlBase}/user/following/${username}`)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .then((response) => {
-          expect(response.status).to.eql(statusCode.NO_CONTENT);
-          expect(response.body).to.eql({});
-        }));
+    let followQuery;
 
-    it('then vefiry idempotent method', () =>
-      agent.put(`${urlBase}/user/following/${username}`)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .then((response) => {
-          expect(response.status).to.eql(statusCode.NO_CONTENT);
-          expect(response.body).to.eql({});
-        }));
+    before(() => {
+      followQuery = agent.put(`${urlBase}/user/following/${username}`)
+        .auth('token', process.env.ACCESS_TOKEN);
+    });
+
+    it('then should be a response', () =>
+      followQuery.then((response) => {
+        expect(response.status).to.eql(statusCode.NO_CONTENT);
+        expect(response.body).to.eql({});
+      }));
 
     describe('when wanna know who follow', () => {
-      it(`then should be followed to ${username}`, () =>
-        agent.get(`${urlBase}/user/following`)
+      let userFollowQuery;
+
+      before(() => {
+        userFollowQuery = agent.get(`${urlBase}/user/following`)
           .auth('token', process.env.ACCESS_TOKEN)
-          .then((response) => {
-            const login = response.body.find(user => user.login === username);
-            assert.exists(login);
-          }));
+          .then(response => response.body.find(user => user.login === username));
+      });
+
+      it(`then should be followed to ${username}`, () =>
+        userFollowQuery.then(user => assert.exists(user)));
+    });
+
+    describe('when wanna follow a user again', () => {
+      let followUserAgainQuery;
+
+      before(() => {
+        followUserAgainQuery = agent.put(`${urlBase}/user/following/${username}`)
+          .auth('token', process.env.ACCESS_TOKEN);
+      });
+
+      it('then verify the method is idempotent', () => {
+        followUserAgainQuery.then((response) => {
+          expect(response.status).to.eql(statusCode.NO_CONTENT);
+          expect(response.body).to.eql({});
+        });
+
+        describe('when wanna know who follow', () => {
+          let userFollowQuery;
+
+          before(() => {
+            userFollowQuery = agent.get(`${urlBase}/user/following`)
+              .auth('token', process.env.ACCESS_TOKEN)
+              .then(response => response.body.find(user => user.login === username));
+          });
+
+          it(`then should be followed to ${username}`, () =>
+            userFollowQuery.then(user => assert.exists(user)));
+        });
+      });
     });
   });
 });
