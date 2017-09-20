eeb3fbcaffb587b6929c998213a17b51ed651e8f
Alejandro Perdomo <aperdomobo@gmail.com>
2017-09-20 10:02:29 -0500

    separate schema from test

--- test/GithubApi.Contract.test.js
+++ test/GithubApi.Contract.test.js
@@ -1,5 +1,6 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
 const chai = require('chai');
+const { listPublicEventsSchema } = require('./schema/ListPublicEvents.schema');
 
 const { expect } = chai;
 chai.use(require('chai-json-schema'));
@@ -8,80 +9,6 @@ const urlBase = 'https://api.github.com';
 
 describe('Given event Github API resources', () => {
   describe('When wanna verify the List public events', () => {
-    const listPublicEventsSchema = {
-      type: 'object',
-      properties: {
-        status: {
-          type: 'number',
-          enum: [200]
-        },
-        body: {
-          type: 'array',
-          items: {
-            type: 'object',
-            properties: {
-              id: { type: 'string' },
-              public: { type: 'boolean' },
-              created_at: { type: 'string' },
-              type: {
-                type: 'string',
-                enum: ['PushEvent', 'PullRequestEvent', 'MemberEvent', 'CreateEvent', 'DeleteEvent', 'PullRequestReviewCommentEvent', 'IssueCommentEvent', 'WatchEvent', 'IssuesEvent', 'ForkEvent', 'GollumEvent']
-              },
-              actor: {
-                type: 'object',
-                properties: {
-                  id: { type: 'number' },
-                  login: { type: 'string' },
-                  display_login: { type: 'string' },
-                  gravatar_id: { type: 'string' },
-                  url: { type: 'string' },
-                  avatar_url: { type: 'string' }
-                }
-              },
-              repo: {
-                type: 'object',
-                properties: {
-                  id: { type: 'number' },
-                  name: { type: 'string' },
-                  url: { type: 'string' }
-                }
-              },
-              payload: {
-                type: 'object',
-                properties: {
-                  push_id: { type: 'number' },
-                  size: { type: 'number' },
-                  distinct_size: { type: 'number' },
-                  ref: { type: ['null', 'string'] },
-                  head: { type: 'string' },
-                  before: { type: 'string' },
-                  commits: {
-                    type: 'array',
-                    items: {
-                      type: 'object',
-                      properties: {
-                        sha: { type: 'string' },
-                        message: { type: 'string' },
-                        distinct: { type: 'boolean' },
-                        url: { type: 'string' },
-                        author: {
-                          type: 'object',
-                          properties: {
-                            email: { type: 'string' },
-                            name: { type: 'string' }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          },
-          required: ['status', 'body']
-        }
-      }
-    };
 
     let listPublicEventsQuery;
 
