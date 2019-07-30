5ba537afb389103f26a64d8cd1f3c054cac1aacf
Alejandro Perdomo <aperdomobo@gmail.com>
2019-07-30 11:43:09 -0500

    update authentication

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -8,22 +8,22 @@ const repository = 'workshop-api-testing-js';
 
 describe('Github Api Test', () => {
   describe('Authentication', () => {
-    it('Via OAuth2 Tokens by Header', () =>
-      agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+    it('Via OAuth2 Tokens by Header', async () => {
+      const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
         .auth('token', process.env.ACCESS_TOKEN)
-        .set('User-Agent', 'agent')
-        .then((response) => {
-          expect(response.status).to.equal(statusCode.OK);
-          expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
-        }));
-  });
+        .set('User-Agent', 'agent');
 
-  it('Via OAuth2 Tokens by parameter', () =>
-    agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
-      .set('User-Agent', 'agent')
-      .query(`access_token=${process.env.ACCESS_TOKEN}`)
-      .then((response) => {
-        expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
-      }));
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
+    });
+
+    it('Via OAuth2 Tokens by parameter', async () => {
+      const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+        .set('User-Agent', 'agent')
+        .query(`access_token=${process.env.ACCESS_TOKEN}`)
+
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
+    });
+  });
 });
