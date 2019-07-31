8ad23642463413f19e001d45bff110be4b845877
Alejandro Perdomo <aperdomobo@gmail.com>
2019-07-31 10:17:23 -0500

    update put tests

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -7,62 +7,59 @@ describe('Given a user github logged', () => {
   const username = 'germandavid85';
 
   describe('when wanna follow a user', () => {
-    let followQuery;
+    let followQueryResponse;
 
-    before(() => {
-      followQuery = agent.put(`${urlBase}/user/following/${username}`)
+    before(async () => {
+      followQueryResponse = await agent.put(`${urlBase}/user/following/${username}`)
         .set('User-Agent', 'agent')
         .auth('token', process.env.ACCESS_TOKEN);
     });
 
-    it('then should be a response', () =>
-      followQuery.then((response) => {
-        expect(response.status).to.eql(statusCode.NO_CONTENT);
-        expect(response.body).to.eql({});
-      }));
+    it('then should be a response', () => {
+      expect(followQueryResponse.status).to.eql(statusCode.NO_CONTENT);
+      expect(followQueryResponse.body).to.eql({});
+    });
 
     describe('when wanna know who follow', () => {
-      let userFollowQuery;
+      let user;
 
-      before(() => {
-        userFollowQuery = agent.get(`${urlBase}/user/following`)
+      before(async () => {
+        const response = await agent.get(`${urlBase}/user/following`)
           .auth('token', process.env.ACCESS_TOKEN)
-          .set('User-Agent', 'agent')
-          .then(response => response.body.find(user => user.login === username));
+          .set('User-Agent', 'agent');
+
+        user = response.body.find(list => list.login === username);
       });
 
-      it(`then should be followed to ${username}`, () =>
-        userFollowQuery.then(user => assert.exists(user)));
+      it(`then should be followed to ${username}`, () => assert.exists(user));
     });
 
     describe('when wanna follow a user again', () => {
-      let followUserAgainQuery;
+      let followUserAgainQueryResponse;
 
-      before(() => {
-        followUserAgainQuery = agent.put(`${urlBase}/user/following/${username}`)
+      before(async () => {
+        followUserAgainQueryResponse = await agent.put(`${urlBase}/user/following/${username}`)
           .set('User-Agent', 'agent')
           .auth('token', process.env.ACCESS_TOKEN);
       });
 
       it('then verify the method is idempotent', () => {
-        followUserAgainQuery.then((response) => {
-          expect(response.status).to.eql(statusCode.NO_CONTENT);
-          expect(response.body).to.eql({});
+        expect(followUserAgainQueryResponse.status).to.eql(statusCode.NO_CONTENT);
+        expect(followUserAgainQueryResponse.body).to.eql({});
+      });
+
+      describe('when wanna know who follow', () => {
+        let user;
+
+        before(async () => {
+          const userFollowQuery = await agent.get(`${urlBase}/user/following`)
+            .set('User-Agent', 'agent')
+            .auth('token', process.env.ACCESS_TOKEN);
+
+          user = userFollowQuery.body.find(list => list.login === username);
         });
 
-        describe('when wanna know who follow', () => {
-          let userFollowQuery;
-
-          before(() => {
-            userFollowQuery = agent.get(`${urlBase}/user/following`)
-              .set('User-Agent', 'agent')
-              .auth('token', process.env.ACCESS_TOKEN)
-              .then(response => response.body.find(user => user.login === username));
-          });
-
-          it(`then should be followed to ${username}`, () =>
-            userFollowQuery.then(user => assert.exists(user)));
-        });
+        it(`then should be followed to ${username}`, () => assert.exists(user));
       });
     });
   });
